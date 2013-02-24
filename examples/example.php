6ec4beb2dcf9af139f2b194354d5193e50ade5b4
sbtkdk <npb@sbtk.dk>
2013-02-24 02:27:18 +0100

    Update examples/example.php

--- examples/example.php
+++ examples/example.php
@@ -19,8 +19,8 @@ require '../src/facebook.php';
 
 // Create our Application instance (replace this with your appId and secret).
 $facebook = new Facebook(array(
-  'appId'  => '344617158898614',
-  'secret' => '6dc8ac871858b34798bc2488200e503d',
+  'appId'  => '269184809826724',
+  'secret' => '60fe8004505eb8602108370e5d425088',
 ));
 
 // Get User ID
